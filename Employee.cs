884513b82bdde0541564d97cbbb43925fff8e32d
CHANDRA BOSE <CHANDRA BOSE@LAPTOP-0D044MST>
2024-09-29 19:32:48 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationPOC
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Department EmpDepartment { get; set; }
+
+        public Employee(string name, Department department)
+        {
+            Name = name;
+            EmpDepartment = department;
+        }
+
+        
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee: {Name}, Department: {EmpDepartment}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace EnumerationPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Employee emp1 = new Employee("Chiluka", Department.HR);
+            Employee emp2 = new Employee("Chandra", Department.IT);
+            Employee emp3 = new Employee("Bose", Department.Sales);
+
+            
+            emp1.DisplayEmployeeInfo();
+            emp2.DisplayEmployeeInfo();
+            emp3.DisplayEmployeeInfo();
+
+            
+            Console.WriteLine("\n--- Department Specific Tasks ---");
+            AssignDepartmentTasks(emp1.EmpDepartment);
+            AssignDepartmentTasks(emp2.EmpDepartment);
+            AssignDepartmentTasks(emp3.EmpDepartment);
+
+            
+            Console.WriteLine("\n--- List of Departments ---");
+            foreach (var dept in Enum.GetValues(typeof(Department)))
+            {
+                Console.WriteLine(dept);
+            }
+        }
+
+        public static void AssignDepartmentTasks(Department department)
+        {
+            switch (department)
+            {
+                case Department.HR:
+                    Console.WriteLine("HR Department: Responsible for employee management and recruitment.");
+                    break;
+                case Department.IT:
+                    Console.WriteLine("IT Department: Responsible for technical support and infrastructure.");
+                    break;
+                case Department.Sales:
+                    Console.WriteLine("Sales Department: Responsible for driving sales and customer relationships.");
+                    break;
+                case Department.Finance:
+                    Console.WriteLine("Finance Department: Responsible for managing company finances.");
+                    break;
+                default:
+                    Console.WriteLine("Unknown Department.");
+                    break;
+            }
+        }
+
+    }
+}
